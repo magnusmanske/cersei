2b5383d2645905919c20883a010b12e653818196
Magnus Manske <magnusmanske@googlemail.com>
2024-03-15 13:22:02 +0000

    misc

--- cersei.py
+++ cersei.py
@@ -1,114 +1,116 @@
 #!/usr/bin/env python3
 
-import sys
 import importlib
-from src.scraper_base import DummyScraper
-from src.artworks import Artworks
-import json # For test123
+import json  # For test123
+import sys
+
 import toolforge  # For oneoff TESTING FIXME
-from src.tooldatabase import ToolDatabase # For oneoff TESTING FIXME
-from src.entry import Entry # For oneoff TESTING FIXME
+
+from src.artworks import Artworks
+from src.entry import Entry  # For oneoff TESTING FIXME
+from src.scraper_base import DummyScraper
+from src.tooldatabase import ToolDatabase  # For oneoff TESTING FIXME
+
 
 def get_scraper_from_id(scraper_id, allow_dummy=False):
-	try:
-		module_name = "src.scrapers.scraper_"+str(scraper_id)
-		class_name = "Scraper"+str(scraper_id)
-		module = importlib.import_module(module_name)
-		class_ = getattr(module, class_name)
-		scraper = class_()
-		return scraper
-	except:
-		if allow_dummy:
-			scraper = DummyScraper(scraper_id)
-			return scraper
-		print(f"No or broken scraper found for #{scraper_id}")
-		exit(0)
+    try:
+        module_name = "src.scrapers.scraper_" + str(scraper_id)
+        class_name = "Scraper" + str(scraper_id)
+        module = importlib.import_module(module_name)
+        class_ = getattr(module, class_name)
+        scraper = class_()
+        return scraper
+    except:
+        if allow_dummy:
+            scraper = DummyScraper(scraper_id)
+            return scraper
+        print(f"No or broken scraper found for #{scraper_id}")
+        exit(0)
 
 
 def get_scraper_ids_from_args():
-	scraper_ids = []
-	if len(sys.argv) == 3:
-		scraper_ids = [int(sys.argv[2])]
-	else:
-		db = ToolDatabase()
-		for scraper in db.query_scrapers():
-			if scraper['property'] is not None:
-				scraper_ids.append(scraper['id'])
-	return scraper_ids
+    scraper_ids = []
+    if len(sys.argv) == 3:
+        scraper_ids = [int(sys.argv[2])]
+    else:
+        db = ToolDatabase()
+        for scraper in db.query_scrapers():
+            if scraper["property"] is not None:
+                scraper_ids.append(scraper["id"])
+    return scraper_ids
 
 
 if __name__ == "__main__":
-	if sys.argv[1] in['run','test']:
-		scraper = get_scraper_from_id(sys.argv[2])
-		if scraper.is_scraper_running():
-			print ("Scraper appears to be running")
-			exit(0)
-		if sys.argv[1]=="run":
-			scraper.get_db().add_log("begin_scrape",scraper.scraper_id)
-		try:
-			scraper.scrape_everything()
-		except:
-			pass
-		if sys.argv[1]=="run":
-			scraper.get_db().add_log("end_scrape",scraper.scraper_id)
-	elif sys.argv[1] == 'new':
-		scraper = get_scraper_from_id(sys.argv[2])
-		if scraper.is_scraper_running():
-			print ("Scraper appears to be running")
-			exit(0)
-		scraper.get_db().add_log("begin_scrape_new",scraper.scraper_id)
-		try:
-			scraper.scrape_new_entries()
-		except:
-			pass
-		scraper.get_db().add_log("end_scrape_new",scraper.scraper_id)
-	elif sys.argv[1] == 'freetext2items':
-		scraper_ids = get_scraper_ids_from_args()
-		for scraper_id in scraper_ids:
-			scraper = get_scraper_from_id(scraper_id,allow_dummy=True)
-			scraper.text2item_heuristic()
-	elif sys.argv[1] == 'clear_scraper_history':
-		scraper = get_scraper_from_id(sys.argv[2],allow_dummy=True)
-		scraper.get_db().add_log("clear_old_revisions",scraper.scraper_id)
-		scraper.clear_old_revisions()
-	elif sys.argv[1] == 'update_from_wikidata':
-		scraper_ids = get_scraper_ids_from_args()
-		for scraper_id in scraper_ids:
-			try:
-				scraper = get_scraper_from_id(scraper_id,allow_dummy=True)
-				scraper.update_from_wikidata()
-			except Exception as err:
-				print(f"Error: {err}")
-	elif sys.argv[1] == 'artwork2qs':
-		artworks = Artworks()
-		artworks.verbose = False
-		qs_commands = artworks.generate_qs()
-		print (qs_commands)
-		#artworks.run_qs(qs_commands)
-	elif sys.argv[1] == 'fill_missing_revision_items':
-		db = ToolDatabase()
-		with db.get_cursor() as cursor:
-			sql = "SELECT id,scraper_id,current_revision_id FROM entry WHERE current_revision_id NOT IN (SELECT revision_id FROM revision_item)"
-			cursor.execute(sql, [])
-			rows = cursor.fetchall()
-		for row in rows:
-			scraper_id = row[1]
-			revision_id = row[2]
-			entry = Entry(scraper_id)
-			entry.id = row[0]
-			entry.load_from_revision(db,revision_id)
-			json = entry.as_json(True)
-			db.set_revision_item(revision_id,json)
-	elif sys.argv[1] == 'test123':
-		db = ToolDatabase()
-		scrapers = db.query_scrapers()
-		print (scrapers)
-		# entry = Entry(12)
-		# entry.load_from_revision(db, 647083)
-		# print (entry.values)
-	else:
-		print (f"Unknown action {sys.argv[1]}")
-
+    if sys.argv[1] in ["run", "test"]:
+        scraper = get_scraper_from_id(sys.argv[2])
+        if scraper.is_scraper_running():
+            print("Scraper appears to be running")
+            exit(0)
+        if sys.argv[1] == "run":
+            scraper.get_db().add_log("begin_scrape", scraper.scraper_id)
+        try:
+            scraper.scrape_everything()
+        except:
+            pass
+        if sys.argv[1] == "run":
+            scraper.get_db().add_log("end_scrape", scraper.scraper_id)
+    elif sys.argv[1] == "new":
+        scraper = get_scraper_from_id(sys.argv[2])
+        if scraper.is_scraper_running():
+            print("Scraper appears to be running")
+            exit(0)
+        scraper.get_db().add_log("begin_scrape_new", scraper.scraper_id)
+        try:
+            scraper.scrape_new_entries()
+        except:
+            pass
+        scraper.get_db().add_log("end_scrape_new", scraper.scraper_id)
+    elif sys.argv[1] == "freetext2items":
+        scraper_ids = get_scraper_ids_from_args()
+        for scraper_id in scraper_ids:
+            scraper = get_scraper_from_id(scraper_id, allow_dummy=True)
+            scraper.text2item_heuristic()
+    elif sys.argv[1] == "clear_scraper_history":
+        scraper = get_scraper_from_id(sys.argv[2], allow_dummy=True)
+        scraper.get_db().add_log("clear_old_revisions", scraper.scraper_id)
+        scraper.clear_old_revisions()
+    elif sys.argv[1] == "update_from_wikidata":
+        scraper_ids = get_scraper_ids_from_args()
+        for scraper_id in scraper_ids:
+            try:
+                scraper = get_scraper_from_id(scraper_id, allow_dummy=True)
+                scraper.update_from_wikidata()
+            except Exception as err:
+                print(f"Error: {err}")
+    elif sys.argv[1] == "artwork2qs":
+        artworks = Artworks()
+        artworks.verbose = False
+        qs_commands = artworks.generate_qs()
+        print(qs_commands)
+        # artworks.run_qs(qs_commands)
+    elif sys.argv[1] == "fill_missing_revision_items":
+        db = ToolDatabase()
+        with db.get_cursor() as cursor:
+            sql = "SELECT id,scraper_id,current_revision_id FROM entry WHERE current_revision_id NOT IN (SELECT revision_id FROM revision_item)"
+            cursor.execute(sql, [])
+            rows = cursor.fetchall()
+        for row in rows:
+            scraper_id = row[1]
+            revision_id = row[2]
+            entry = Entry(scraper_id)
+            entry.id = row[0]
+            entry.load_from_revision(db, revision_id)
+            json = entry.as_json(True)
+            db.set_revision_item(revision_id, json)
+    elif sys.argv[1] == "test123":
+        db = ToolDatabase()
+        scrapers = db.query_scrapers()
+        print(scrapers)
+        # entry = Entry(12)
+        # entry.load_from_revision(db, 647083)
+        # print (entry.values)
+    else:
+        print(f"Unknown action {sys.argv[1]}")
 
 
 """ Last scraper runtimes
--- src/scrapers/scraper_1.py
+++ src/scrapers/scraper_1.py
@@ -1,99 +1,114 @@
-from src.scraper_base import ScraperBase
-from bs4 import BeautifulSoup
-import requests
 import re
-from src.values import TimeValue
+
+import requests
+from bs4 import BeautifulSoup
+
 from src.entry import Entry
+from src.scraper_base import ScraperBase
+from src.values import TimeValue
+
 
 class Scraper1(ScraperBase):
+    """Slovnikceske Literatury CZ"""
 
-	"""Slovnikceske Literatury CZ
-	"""
-	def __init__(self):
-		super().__init__(1)
+    def __init__(self):
+        super().__init__(1)
 
-	def scrape_everything(self):
-		self.scrape_everything_via_index()
+    def scrape_everything(self):
+        self.scrape_everything_via_index()
 
-	def paginate_index(self):
-		pageNr = 0
-		while True:
-			try:
-				url = "http://slovnikceskeliteratury.cz/list.jsp?show=-&order=title&ascDesc=asc&startswith="
-				page = requests.post(url, data = {"page":1,"pageNr":pageNr}, timeout=60)
-				html = page.text
-				yield html
-				soup = BeautifulSoup(html,features="html.parser")
-				if len(soup.find_all('input', attrs={"class": "submitButton", "name": "next"}))==0:
-					break # No "next" button, quit
-			except Exception as err:
-				print(f"Unexpected {err}")
-			pageNr += 1
+    def paginate_index(self):
+        pageNr = 0
+        while True:
+            try:
+                url = "http://slovnikceskeliteratury.cz/list.jsp?show=-&order=title&ascDesc=asc&startswith="
+                page = requests.post(
+                    url, data={"page": 1, "pageNr": pageNr}, timeout=60
+                )
+                html = page.text
+                yield html
+                soup = BeautifulSoup(html, features="html.parser")
+                if (
+                    len(
+                        soup.find_all(
+                            "input", attrs={"class": "submitButton", "name": "next"}
+                        )
+                    )
+                    == 0
+                ):
+                    break  # No "next" button, quit
+            except Exception as err:
+                print(f"Unexpected {err}")
+            pageNr += 1
 
-	def entry_url_relative2full(self,url):
-		return re.sub(r'^\./showContent\.jsp','http://www.slovnikceskeliteratury.cz/showContent.jsp',url)
+    def entry_url_relative2full(self, url):
+        return re.sub(
+            r"^\./showContent\.jsp",
+            "http://www.slovnikceskeliteratury.cz/showContent.jsp",
+            url,
+        )
 
-	def parse_index_page(self,html):
-		soup = BeautifulSoup(html,features="html.parser")
-		for div in soup.find_all('div', class_="result"):
-			entry = Entry(self.scraper_id)
-			descs = []
-			is_human = False
-			for link in div.find_all('a', class_="result_name"):
-				original_label = link.get_text().strip()
-				pretty_label = original_label
-				pretty_label = re.sub(r'^.+? - +','',pretty_label)
-				pretty_label = re.sub(r'\s*\(.*$','',pretty_label)
-				pretty_label = re.sub(r'^(.+?), (.+)$',r'\2 \1',pretty_label)
-				entry.add_label_etc(original_label,"original_label",self.language)
-				entry.add_label_etc(pretty_label,"label",self.language)
-				href = link.get('href')
-				if href is not None:
-					m = re.match(r"^.*docId=(\d+).*$",href)
-					if m:
-						entry.id = m.group(1)
-						url = self.construct_entry_url_from_id(entry.id)
-						entry.add_label_etc(url,"url",self.language)
-			for span in div.find_all('span', class_="datumnarozeni"):
-				s = re.sub(r"\s+"," ",span.get_text()).strip()
-				if self.parse_date_prop(entry,s,"P569"):
-					is_human = True
-				descs.append(s)
-			for span in div.find_all('span', class_="mistonarozeni"):
-				s = re.sub(r"\s+"," ",span.get_text()).strip()
-				self.add_to_item_or_freetext("P19",s,entry)
-				descs.append(s)
-			for span in div.find_all('span', class_="datumumrti"):
-				s = re.sub(r"\s+"," ",span.get_text()).strip()
-				if self.parse_date_prop(entry,s,"P570"):
-					is_human = True
-				descs.append(s)
-			for span in div.find_all('span', class_="mistoumrti"):
-				s = re.sub(r"\s+"," ",span.get_text()).strip()
-				self.add_to_item_or_freetext("P20",s,entry)
-				descs.append(s)
-			self.add_description_list(descs,entry)
-			if is_human:
-				entry.add_item("P31","Q5")
-			if entry.is_valid():
-				yield entry
+    def parse_index_page(self, html):
+        soup = BeautifulSoup(html, features="html.parser")
+        for div in soup.find_all("div", class_="result"):
+            entry = Entry(self.scraper_id)
+            descs = []
+            is_human = False
+            for link in div.find_all("a", class_="result_name"):
+                original_label = link.get_text().strip()
+                pretty_label = original_label
+                pretty_label = re.sub(r"^.+? - +", "", pretty_label)
+                pretty_label = re.sub(r"\s*\(.*$", "", pretty_label)
+                pretty_label = re.sub(r"^(.+?), (.+)$", r"\2 \1", pretty_label)
+                entry.add_label_etc(original_label, "original_label", self.language)
+                entry.add_label_etc(pretty_label, "label", self.language)
+                href = link.get("href")
+                if href is not None:
+                    m = re.match(r"^.*docId=(\d+).*$", href)
+                    if m:
+                        entry.id = m.group(1)
+                        url = self.construct_entry_url_from_id(entry.id)
+                        entry.add_label_etc(url, "url", self.language)
+            for span in div.find_all("span", class_="datumnarozeni"):
+                s = re.sub(r"\s+", " ", span.get_text()).strip()
+                if self.parse_date_prop(entry, s, "P569"):
+                    is_human = True
+                descs.append(s)
+            for span in div.find_all("span", class_="mistonarozeni"):
+                s = re.sub(r"\s+", " ", span.get_text()).strip()
+                self.add_to_item_or_freetext("P19", s, entry)
+                descs.append(s)
+            for span in div.find_all("span", class_="datumumrti"):
+                s = re.sub(r"\s+", " ", span.get_text()).strip()
+                if self.parse_date_prop(entry, s, "P570"):
+                    is_human = True
+                descs.append(s)
+            for span in div.find_all("span", class_="mistoumrti"):
+                s = re.sub(r"\s+", " ", span.get_text()).strip()
+                self.add_to_item_or_freetext("P20", s, entry)
+                descs.append(s)
+            self.add_description_list(descs, entry)
+            if is_human:
+                entry.add_item("P31", "Q5")
+            if entry.is_valid():
+                yield entry
 
-	def parse_date_prop(self,entry,date_string,prop):
-		date_string = date_string.strip()
-		if date_string=="":
-			return False
-		try:
-			m = re.match(r"^[*†]\D*(\d{1,2})\. *(\d{1,2})\. *(\d{3,4})",date_string)
-			if m:
-				day = int(m.group(1))
-				month = int(m.group(2))
-				year = int(m.group(3))
-				tv = TimeValue(ymd=(year, month, day), precision=11)
-				entry.add_time(prop,tv)
-				return True
-			else: # Couldn't parse date
-				entry.add_freetext(prop,date_string)
-				return False
-		except: # Couldn't parse date
-			entry.add_freetext(prop,date_string)
-			return False
+    def parse_date_prop(self, entry, date_string, prop):
+        date_string = date_string.strip()
+        if date_string == "":
+            return False
+        try:
+            m = re.match(r"^[*†]\D*(\d{1,2})\. *(\d{1,2})\. *(\d{3,4})", date_string)
+            if m:
+                day = int(m.group(1))
+                month = int(m.group(2))
+                year = int(m.group(3))
+                tv = TimeValue(ymd=(year, month, day), precision=11)
+                entry.add_time(prop, tv)
+                return True
+            else:  # Couldn't parse date
+                entry.add_freetext(prop, date_string)
+                return False
+        except:  # Couldn't parse date
+            entry.add_freetext(prop, date_string)
+            return False
